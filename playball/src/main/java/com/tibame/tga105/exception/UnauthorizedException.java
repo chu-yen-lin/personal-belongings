d32c9ca50cf3e78ce72b636e197228855e973e17
chu-yen-lin <snak2300@yahoo.com.tw>
2024-12-23 15:02:00 +0800

    移除已跟踪的 playdrums和playball文件夹且更新 .gitignore

--- playball/src/main/java/com/tibame/tga105/exception/UnauthorizedException.java
+++
@@ -1,8 +0,0 @@
-package com.tibame.tga105.exception;
-
-// 自定義非授權例外
-public class UnauthorizedException extends Exception {
-	public UnauthorizedException(String message) {
-		super(message);
-	}
-}
--- playball/src/main/java/com/tibame/tga105/service/AdminService.java
+++
@@ -1,7 +0,0 @@
-package com.tibame.tga105.service;
-
-import com.tibame.tga105.vo.AdminVO;
-
-public interface AdminService {
-	public AdminVO login(AdminVO adminVO);
-}
